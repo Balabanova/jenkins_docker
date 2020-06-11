817b66ad6a4bb9403f5f37af77ae004c838ec3f3
Balabanova <balabanova_elia@mail.ru>
2020-06-11 14:49:24 +0500

    Update test.groovy

--- jenkinsfile/test.groovy
+++ jenkinsfile/test.groovy
@@ -10,12 +10,22 @@ pipeline {
                               branches                         : [[name: '*/master']],
                               doGenerateSubmoduleConfigurations: false,
                               extensions                       : [[$class           : 'RelativeTargetDirectory',
-                                                                   relativeTargetDir: 'auto']],
+                                                                   relativeTargetDir: 'GitDir']],
                               submoduleCfg                     : [],
                               userRemoteConfigs                : [[credentialsId: 'LizaBalabanovaGit', url: 'https://github.com/Balabanova/jenkins_docker.git']]])
                 }
             }
         }     
+        
+         stage ('Create docker image'){
+            steps{
+                script{
+                    sh "docker build ${WORKSPACE}/GitDir -t webapp"
+                    sh "docker run -d webapp"
+                    sh "docker exec -it webapp "df -h > ~/proc""
+                }
+            }
+        }
         
     }   
 }
