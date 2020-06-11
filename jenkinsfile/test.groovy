2d1415d591a18805e7197357f1563bea544c97f8
Balabanova <balabanova_elia@mail.ru>
2020-06-11 13:10:45 +0500

    Create test.groovy

---
+++ jenkinsfile/test.groovy
@@ -0,0 +1,33 @@
+pipeline {
+    agent{node('master')}
+    stages {
+        
+        stage('Dowload project') {
+            steps {
+                
+                script {cleanWs()}
+                
+                script {
+                    echo 'Start download project'
+                    checkout([$class                           : 'GitSCM',
+                              branches                         : [[name: '*/master']],
+                              doGenerateSubmoduleConfigurations: false,
+                              extensions                       : [[$class           : 'RelativeTargetDirectory',
+                                                                   relativeTargetDir: 'auto']],
+                              submoduleCfg                     : [],
+                              userRemoteConfigs                : [[credentialsId: 'LizaBalabanovaGit', url: 'https://github.com/Balabanova/jenkins_docker.git']]])
+                }
+            }
+        }
+        
+        stage ('Create docker image'){
+            steps{
+                script{
+                    sh "docker build ${WORKSPACE}/auto -t webapp"
+                    sh "docker run -d webapp"
+                    sh "docker exec -it webapp "df -h > ~/proc""
+                }
+            }
+        }    
+    }   
+}
