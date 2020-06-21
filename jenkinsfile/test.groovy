eccb635cf52dcbdda7d29d8bc8071f869725298d
Balabanova <balabanova_elia@mail.ru>
2020-06-21 19:33:44 +0500

    Update test.groovy

--- jenkinsfile/test.groovy
+++ jenkinsfile/test.groovy
@@ -3,14 +3,28 @@ pipeline {
     stages { 
         stage('Dowload project') {
             steps { 
-                script {cleanWs()}
+                script {
+                    cleanWs()
+                    withCredentials([
+                        usernamePassword(credentialsId: 'srv_sudo',
+                        usernameVariable: 'username',
+                        passwordVariable: 'password')
+                    ]) {
+                        try {
+                            sh "echo '${password}' | sudo -S docker stop v_name"
+                            sh "echo '${password}' | sudo -S docker container rm v_name"
+                        } catch (Exception e) {
+                            print 'container not exist, skip clean'
+                        }
+                    }
+                }
                 script {
                     echo 'Start boot process...'
                     checkout([$class                           : 'GitSCM',
                               branches                         : [[name: '*/master']],
                               doGenerateSubmoduleConfigurations: false,
                               extensions                       : [[$class           : 'RelativeTargetDirectory',
-                                                                   relativeTargetDir: 'GitDir']],
+                                                                   relativeTargetDir: 'auto']],
                               submoduleCfg                     : [],
                               userRemoteConfigs                : [[credentialsId: 'LizaBalabanovaGit', url: 'https://github.com/Balabanova/jenkins_docker.git']]])
                 }
@@ -20,12 +34,37 @@ pipeline {
          stage ('Create docker image'){
             steps{
                 script{
-                    sh "docker build ${WORKSPACE}/GitDir -t webapp"
-                    sh "docker run -d webapp"
-                    sh "docker exec -it webapp "df -h > ~/proc""
+                    withCredentials([
+                        usernamePassword(credentialsId: 'srv_sudo',
+                        usernameVariable: 'username',
+                        passwordVariable: 'password')
+                    ]) {
+
+                        sh "echo '${password}' | sudo -S docker build ${WORKSPACE}/auto -t v_nginx"
+                        sh "echo '${password}' | sudo -S docker run -d -p 8008:80 --name v_name -v /home/adminci/v_dir:/stat v_nginx"
+                    }
+                    //sh "docker build ${WORKSPACE}/GitDir -t webapp"
+                    //sh "docker run -d webapp"
+                    //sh "docker exec -it webapp "df -h > ~/proc""
+                }
+            }
+        }      
+    } 
+    
+    stage ('Write to file'){
+            steps{
+                script{
+                    withCredentials([
+                        usernamePassword(credentialsId: 'srv_sudo',
+                        usernameVariable: 'username',
+                        passwordVariable: 'password')
+                    ]) {
+                        
+                        sh "echo '${password}' | sudo -S docker exec -t isng bash -c 'df -h > /stat/v_file.txt'"
+                        sh "echo '${password}' | sudo -S docker exec -t isng bash -c 'top -n 1 -b >> /stat/v_file.txt'"
+                    }
                 }
             }
         }
-        
-    }   
+    }
 }
